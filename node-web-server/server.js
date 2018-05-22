bcdbc502addbe24a2336242d2ca25b6c5b08da46
mrik <i852933@PALM00621563A.ariba.com>
2018-05-22 13:34:08 -0700

    some changes

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('error');
+        }
+    });
+    next();
+});
 
 app.get('/', (req, res) => {
     res.render('home.hbs',{
